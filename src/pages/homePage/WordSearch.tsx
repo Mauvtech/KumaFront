cfc2a6103e95065661f09dc1133242fbbf14c441
Iliès BELDJILALI <ilies.beldjilali@cartier.com>
2024-08-26 03:21:20 +0200

    chore: merge search and filters

--- src/pages/homePage/WordSearch.tsx
+++ src/pages/homePage/WordSearch.tsx
@@ -1,45 +1,31 @@
 import Input from "../../components/Common/Input";
 import Selector from "../../components/Common/Selector";
 import React from "react";
+import {HomePageFilters} from "./HomePage";
 
 export default function WordSearch({
-                                       setSearchTerm,
+                                       filters,
+                                       setFilters,
                                        setCurrentPage,
                                        setTerms,
                                        setAllFetchedTerms,
-                                       searchTerm,
                                        categories,
                                        themes,
-
                                        languages,
-                                       selectedCategory,
-                                       selectedTheme,
-                                       selectedLanguage,
-                                       setSelectedCategory,
-                                       setSelectedTheme,
-                                       setSelectedLanguage,
-
                                    }: {
-
-    setSearchTerm: (term: string) => void;
+    filters: HomePageFilters,
+    setFilters: (filters: HomePageFilters) => void;
     setCurrentPage: (page: number) => void;
     setTerms: (terms: any[]) => void;
     setAllFetchedTerms: (terms: any[]) => void;
-    searchTerm: string;
     categories: any[];
     themes: any[];
     languages: any[];
-    selectedCategory: string;
-    selectedTheme: string;
-    selectedLanguage: string;
-    setSelectedCategory: (category: string) => void;
-    setSelectedTheme: (theme: string) => void;
-    setSelectedLanguage: (language: string) => void;
 }) {
 
 
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(e.target.value);
+        setFilters({...filters, searchTerm: e.target.value});
         setCurrentPage(1); // Reset page number when search term changes
         setTerms([]); // Clear current terms
         setAllFetchedTerms([]); // Clear all fetched terms
@@ -47,21 +33,21 @@ export default function WordSearch({
 
 
     const handleCategoryChange = (category: string) => {
-        setSelectedCategory(category === selectedCategory ? "" : category);
+        setFilters({...filters, category});
         setCurrentPage(1); // Reset page number when category changes
         setTerms([]); // Clear current terms
         setAllFetchedTerms([]); // Clear all fetched terms
     };
 
     const handleThemeChange = (theme: string) => {
-        setSelectedTheme(theme === selectedTheme ? "" : theme);
+        setFilters({...filters, theme});
         setCurrentPage(1); // Reset page number when theme changes
         setTerms([]); // Clear current terms
         setAllFetchedTerms([]); // Clear all fetched terms
     };
 
     const handleLanguageChange = (language: string) => {
-        setSelectedLanguage(language === selectedLanguage ? "" : language);
+        setFilters({...filters, language});
         setCurrentPage(1); // Reset page number when language changes
         setTerms([]); // Clear current terms
         setAllFetchedTerms([]); // Clear all fetched terms
@@ -71,7 +57,7 @@ export default function WordSearch({
     return (
         <div className="sm:sticky sm:top-16 bg-background z-10">
             <Input
-                value={searchTerm}
+                value={filters.searchTerm ?? ""}
                 onChange={handleSearchChange}
                 placeholder="Rechercher un terme ou une définition..."
             />
@@ -80,7 +66,7 @@ export default function WordSearch({
                 <div className="relative w-full sm:w-1/3 mb-4 sm:mb-0">
                     <Selector
                         options={categories.map((cat) => cat.name)}
-                        selectedOption={selectedCategory}
+                        selectedOption={filters.category ?? ""}
                         onSelectOption={handleCategoryChange}
                         placeholder="Select Category"
                     />
@@ -88,7 +74,7 @@ export default function WordSearch({
                 <div className="relative w-full sm:w-1/3 mb-4 sm:mb-0">
                     <Selector
                         options={themes.map((theme) => theme.name)}
-                        selectedOption={selectedTheme}
+                        selectedOption={filters.theme ?? ""}
                         onSelectOption={handleThemeChange}
                         placeholder="Select Theme"
                     />
@@ -96,7 +82,7 @@ export default function WordSearch({
                 <div className="relative w-full sm:w-1/3">
                     <Selector
                         options={languages.map((lang) => lang.name)}
-                        selectedOption={selectedLanguage}
+                        selectedOption={filters.language ?? ""}
                         onSelectOption={handleLanguageChange}
                         placeholder="Select Language"
                     />
