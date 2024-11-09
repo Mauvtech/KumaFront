363134e21c3c176eeee5713e98ffc6add75894aa
ilies-bel <beldjilali.ilies@gmail.com>
2024-11-09 17:35:15 +0100

    feat: term details

---
+++ src/components/Modal.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {AnimatePresence, motion} from "framer-motion";
+import {clsx} from "clsx";
+
+export function Modal({children, open, onOk, className}: {
+    children: React.ReactNode,
+    open: boolean,
+    onOk: () => void,
+    className?: string
+}) {
+    return <AnimatePresence>
+        {open && (
+            <motion.div
+                className={clsx("fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75")}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+                transition={{duration: 0.3}}
+            >
+                <motion.div
+                    className={clsx("bg-white p-6 rounded-lg shadow-lg text-center", className)}
+                    initial={{scale: 0.8, opacity: 0}}
+                    animate={{scale: 1, opacity: 1}}
+                    exit={{scale: 0.8, opacity: 0}}
+                    transition={{duration: 0.3}}
+                >
+                    {children}
+                    <motion.button
+                        onClick={onOk}
+
+                        className="px-4 py-2 bg-primaryLight text-gray-600 rounded-lg shadow-neumorphic transition-transform transform hover:scale-105 focus:outline-none"
+                        whileTap={{scale: 0.95}}
+                    >
+                        OK
+                    </motion.button>
+                </motion.div>
+            </motion.div>
+        )}
+    </AnimatePresence>
+}
---
+++ src/components/Terms/TermDetails/TermSummary.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+export type TermSummary = {
+    term: string;
+    definition: string;
+    translation: string;
+    grammaticalCategory: string;
+    language: string;
+    tags: string[];
+}
+
+export function TermSummary({term, definition, grammaticalCategory, tags, language, translation}: TermSummary) {
+
+    return (
+        <>
+            <h2 className="text-2xl font-bold mb-4 text-text">{term}</h2>
+            <div className="flex flex-wrap gap-2 mb-4">
+                        <span className="bg-primaryLight text-primary text-xs px-2 rounded-full">
+                            {grammaticalCategory}
+                        </span>
+                <span className="bg-secondaryLight text-secondary text-xs px-2 rounded-full">
+                            {tags.join()}
+                        </span>
+                <span className="bg-accentLight text-accent text-xs px-2 rounded-full">
+                            {language}
+                        </span>
+            </div>
+            <div className="mb-4 p-4 bg-backgroundHover rounded-lg shadow-neumorphic">
+                <p className="font-semibold text-text">Definition</p>
+                <p>{definition}</p>
+            </div>
+            <div className="mb-4 p-4 bg-backgroundHover rounded-lg shadow-neumorphic">
+                <p className="font-semibold text-text">Translation</p>
+                <p>{translation}</p>
+            </div>
+        </>
+
+    )
+}
